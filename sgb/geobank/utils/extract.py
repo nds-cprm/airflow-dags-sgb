793057494dffb07ed6d8021cfa6171dac6940cdf
cmotadev <cmota.dev@gmail.com>
2023-06-19 18:26:47 -0300

    [Fix] correção na extração de datas.

--- sgb/geobank/utils/extract.py
+++ sgb/geobank/utils/extract.py
@@ -9,7 +9,12 @@ def get_st_geometry_tables(oracle_conn_id, **kwargs):
     """
     start = kwargs.get('dag_run').logical_date
 
-    dest_path = path.join(environ.get("AIRFLOW_HOME", "/tmp"), start.year, start.month, start.day, "st_geometry_tables.pkl")
+    dest_path = path.join(
+        environ.get("AIRFLOW_HOME", "/tmp"), 
+        start.strftime("%Y/%m/%d"), 
+        "st_geometry_tables.pkl"
+    )
+    
     makedirs(dest_path, exist_ok=True)
 
     pkl = path.join(dest_path, "st_geometry_tables.pkl")
