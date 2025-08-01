b59d7faaef6fd965e5d79c32eafadec209260200
cmotadev <cmota.dev@gmail.com>
2025-08-01 18:58:02 -0300

    Primeira implementação de hook com geopandas e reflection

--- sgb/geobank/dags/oracle-test.py
+++ sgb/geobank/dags/oracle-test.py
@@ -10,29 +10,24 @@ from sgb.common.airflow.hooks.sde import SDEOracleHook
 # DAG
 dag_id = "test-oracle"
 
-def sde_version(**kwargs):
-    return SDEOracleHook(oracle_conn_id="geobank-producao").sde_version
-
-
 def extract_table(**kwargs):
-    engine = SDEOracleHook(oracle_conn_id="geobank-producao").get_sqlalchemy_engine()
+    hook = SDEOracleHook(oracle_conn_id="geobank-producao")
+    table = hook.get_reflected_table('litoestratigrafia', 'ue_layer_100000')
+
+    results = hook.get_geopandas_df_by_chunks(
+        str(table.select()), 
+        geom_col=table.columns['shape'].name, 
+        crs=table.columns['shape'].type.srid, 
+        index_col=table.primary_key.columns[0].name,
+        chunksize=5000
+    )
+
+    out_file = "/tmp/ue_layer_100000.parquet"
     
-    with engine.connect() as conn:
-        sql = """SELECT objectid, id_unidade_estratigrafica, sigla, hierarquia, nome, ambiente_tectonico, sub_ambiente_tectonico, 
-              sigla_pai, nome_pai, legenda, escala, mapa, litotipos, range, idade_min, idade_max, eon_min, eon_max, era_min, 
-              era_max, sistema_min, sistema_max, epoca_min, epoca_max, SDE.ST_ASBINARY(shape) AS geometry, siglas_historicas 
-              FROM litoestratigrafia.ue_layer_25000"""  
+    for df in results:
+        df.to_parquet(out_file, index=True)
 
-        geodata = (
-            gpd.read_postgis(sql, conn, geom_col="geometry", crs=4326)
-            .rename(columns=lambda col: col.lower())
-            .rename(columns={"objectid": "fid"})
-            .set_index("fid")
-        )
-
-    geodata.to_parquet("/tmp/ue_layer_25000.parquet", index=True)
-
-    return geodata.info()
+    return out_file
 
 
 with DAG(
@@ -41,17 +36,10 @@ with DAG(
     schedule=None,
     catchup=False
 ) as dag:
-    sde_version = PythonOperator(
-        task_id=f"{dag_id}_sde_version",
-        python_callable=sde_version
-    )
-
     extract_table = PythonOperator(
         task_id=f"{dag_id}_extract_table",
         python_callable=extract_table
-    )
-
-    sde_version >> extract_table
+    ) # type: ignore
 
 if __name__ == '__main__':
     dag.test()
